7097a08d92444b06120899554635f9a64814b74c
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2022-11-26 20:34:45 +0100

    Add a missing version

--- backend/TestFunction.cs
+++ backend/TestFunction.cs
@@ -19,6 +19,7 @@ public class BookVm
     public string Title { get; set; }
     public string Author { get; set; }
     public byte[] Cover { get; set; }
+    public string[] Tags { get; set; }
 }
 
 public class TestFunction
@@ -38,18 +39,31 @@ public class TestFunction
             var connection = new SqlConnection(connectionString);
             const string queryText = @"IF (LEN(@tags) < 1)
             BEGIN
-            SELECT * FROM dbo.Books;
+                SELECT b.*, STUFF(
+                    (SELECT DISTINCT ',' + tg.Name
+                    FROM dbo.Tags tg
+                    INNER JOIN BooksTags BT2 on tg.Id = BT2.TagId
+                    WHERE BT2.BookId = b.Id
+                    FOR XML PATH (''))
+                    , 1, 1, '')  AS TagsArray 
+                FROM dbo.Books b
+                INNER JOIN BooksTags BT on b.Id = BT.BookId
+                group by b.Id, b.Title, b.Author, b.Cover
             END
                 ELSE
             BEGIN
-                select b.*
+                select b.*, STUFF(
+                    (SELECT DISTINCT ',' + tg.Name
+                     FROM dbo.Tags tg
+                     INNER JOIN BooksTags BT2 on tg.Id = BT2.TagId
+                     WHERE BT2.BookId = b.Id
+                     FOR XML PATH (''))
+                     , 1, 1, '')  AS TagsArray
                 from dbo.Tags t
-            inner join BooksTags bt on bt.TagId = t.Id
-            inner join Books b on b.Id = bt.BookId
-            inner join String_split(@tags, ',') ts on ts.value = t.Name
-            group by b.Id, b.Title, b.Author, b.Cover
-                having count(*) >= @minTags
-            order by b.Title, b.Author;
+                inner join BooksTags bt on bt.TagId = t.Id
+                inner join Books b on b.Id = bt.BookId
+                inner join String_split(@tags, ',') ts on RTRIM(LTRIM(ts.value)) = t.Name
+                group by b.Id, b.Title, b.Author, b.Cover
             END";
             connection.Open();
 
@@ -69,7 +83,8 @@ public class TestFunction
                     Id = reader.GetInt32(0),
                     Title = reader.GetString(1),
                     Author = reader.GetString(2),
-                    Cover = reader.GetSqlBytes(3).Buffer
+                    Cover = reader.GetSqlBytes(3).Buffer,
+                    Tags = reader.GetString(4).Split(",")
                 });
 
                 log.LogInformation("Response: {Response}", response.ToString());
